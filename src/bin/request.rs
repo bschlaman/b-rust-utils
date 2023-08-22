ceee58d25fbf8a560b2e097c997908ba14e142ea
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-08-21 23:05:19 -0700

    move script and update dependencies

--- src/bin/request.rs
+++ src/bin/request.rs
@@ -1,7 +1,7 @@
-use log::{error, info, warn, LevelFilter};
 use simple_logger::SimpleLogger;
 use std::env;
 use std::io::Read;
+use std::process::exit;
 
 struct ResponseData {
     http_status_code: u16,
@@ -12,36 +12,39 @@ fn perform_get_request(url: &String) -> Result<ResponseData, reqwest::Error> {
     let mut res = reqwest::blocking::get(url)?;
     let mut body = String::new();
     res.read_to_string(&mut body).ok();
-    return Ok(ResponseData {
+
+    Ok(ResponseData {
         http_status_code: res.status().as_u16(),
         body_length: body.len(),
-    });
+    })
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     SimpleLogger::new()
-        .with_level(LevelFilter::Debug)
+        .with_level(log::LevelFilter::Debug)
         .init()
         .unwrap();
 
-    info!("starting script!");
-    warn!("I suck at rust!");
+    log::info!("starting script!");
+    log::warn!("I suck at rust!");
 
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        error!("too few args bro");
-        panic!();
+        log::error!("too few args");
+        exit(1);
     }
 
     let filename = &args[0];
     let url = &args[1];
 
-    info!("script name: {}", filename);
-    info!("url passed: {}", url);
+    log::info!("script name: {}", filename);
+    log::info!("url passed:  {}", url);
 
     let res_data = perform_get_request(url).unwrap();
-    info!(
+    log::info!(
         "http status code: {}, length of response body: {}",
-        res_data.http_status_code, res_data.body_length,
+        res_data.http_status_code,
+        res_data.body_length,
     );
 }
