d666c9486d08dcd835a5e63932ede44d4629da46
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-08-28 22:56:26 +0000

    improve format

--- src/bin/btime.rs
+++ src/bin/btime.rs
@@ -1,30 +1,30 @@
 use chrono::{Local, Utc};
 use colored::Colorize;
 
-
 fn print_dates_with_emojis() {
     let utc = Utc::now();
     let local = Local::now();
-    
-    let utc_iso_formatted = utc.to_rfc3339();
+
     let unix = local.timestamp();
+    let utc_iso_formatted = utc.to_rfc3339();
 
     let utc_formatted = utc.format("%Y-%m-%d %H:%M");
+    let iso_date = utc.format("%Y.%m.%d");
     let utc_friendly = utc.format("%a %b %e %H:%M %Z");
     let local_formatted = local.format("%Y-%m-%d %H:%M");
-    
+
     let package_name = env!("CARGO_PKG_NAME");
 
     println!("== {} ==", package_name.bold());
     println!("{}", unix.to_string().yellow());
     println!("{}", utc_iso_formatted.blue());
+    println!("{}", iso_date.to_string().bold());
     println!("===========");
-    println!("⌛ {} {}", local_formatted, "(Local)".dimmed());
-    println!("⌛ {} {}", utc_formatted, "(UTC)".dimmed());
-    println!("📅 {} {}", utc_friendly, "(UTC)".dimmed());
+    println!("⌛ {:<20} {}", local_formatted, "(Local)".dimmed());
+    println!("⌛ {:<20} {}", utc_formatted, "(UTC)".dimmed());
+    println!("📅 {:<20} {}", utc_friendly, "(UTC)".dimmed());
 }
 
 fn main() {
     print_dates_with_emojis();
 }
-
