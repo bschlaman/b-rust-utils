ac270594d95745c666fc5d50e5abf86be9cc7831
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-09-06 23:03:25 +0000

    update README and prep requests utility

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 ## Intro
-This is my "hello world" rust project that fetches a url and outputs the HTTP status code and size of the response in JSON
+This package is a toolbox of cli driven utilities written in rust.
+
+The utilities aim to have a minimal interface and are intended for use in modern terminal emulators.
+
+## Utilities
+
+### `btime`
+
+Displays local and utc date + time information in several useful formats.
+
+```console
++------------------------------------------------------------------------------+
+| Description            Local                    UTC                   Length |
++==============================================================================+
+| Package name       📦  b-rust-utils                                          |
+| Unix               ⏰️  1694034020                                     10     |
+|                                                                              |
+| Friendly Calendar  📅  Wed Sep  6 21:00 +00:00  Wed Sep  6 21:00 UTC  20     |
+| RFC 3339           ⌛  2023-09-06T21:00:20Z     2023-09-06T21:00:20Z  20     |
+| ISO 8601           📄  2023-09-06 21:00         2023-09-06 21:00      16     |
+| ISO 8601 Date      💻  2023.09.06               2023.09.06            10     |
++------------------------------------------------------------------------------+
+```
+
+### `request`
+
+**BETA FEATURE**
+
+Essentially a `curl` clone that displays various information about the request.
+
+Example:
+```bash
+request https://www.schlamalama.com
+```
+
--- src/bin/request.rs
+++ src/bin/request.rs
@@ -1,12 +1,12 @@
-use simple_logger::SimpleLogger;
 use colored::Colorize;
+use simple_logger::SimpleLogger;
 
 struct ResponseData {
     http_status_code: u16,
     body_length: usize,
 }
 
-async fn perform_get_request(url: &String) -> Result<ResponseData, reqwest::Error> {
+async fn perform_get_request_async(url: &String) -> Result<ResponseData, reqwest::Error> {
     let res = reqwest::get(url).await?;
     let http_status_code = res.status().as_u16();
     let body = res.text().await?;
@@ -18,13 +18,34 @@ async fn perform_get_request(url: &String) -> Result<ResponseData, reqwest::Erro
 }
 
 #[tokio::main]
-async fn main() {
+async fn main_async() {
+    let url = "";
+
+    let start_time = std::time::Instant::now();
+
+    let res_data = perform_get_request_async(&url.to_string()).await.unwrap();
+
+    let duration = start_time.elapsed().as_millis();
+    log::debug!("time elapsed (ms): {}", duration);
+
+    log::info!(
+        "http status code: {}, length of response body: {}",
+        res_data.http_status_code.to_string().blue(),
+        res_data.body_length.to_string().green(),
+    );
+}
+
+fn main() {
+    setup();
+}
+
+fn setup() {
     SimpleLogger::new()
         .with_level(log::LevelFilter::Debug)
         .init()
         .unwrap();
 
-    let args: Vec<String> = std::env::args().collect();
+    let args = std::env::args().collect::<Vec<String>>();
     if args.len() != 2 {
         log::error!("too few args");
         std::process::exit(1);
@@ -35,15 +56,4 @@ async fn main() {
 
     log::info!("script: {}", filename.bold());
     log::info!("url:    {}", url.bright_yellow());
-
-    let start_time = std::time::Instant::now();
-    let res_data = perform_get_request(url).await.unwrap();
-    let duration = start_time.elapsed().as_millis();
-    log::debug!("time elapsed (ms): {}", duration);
-
-    log::info!(
-        "http status code: {}, length of response body: {}",
-        res_data.http_status_code.to_string().blue(),
-        res_data.body_length.to_string().green(),
-    );
 }
