a3f38ecd5d59e728876df6ecd4085e754e8b91dc
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-09-15 22:36:36 +0000

    prettier float printing

--- src/bin/math.rs
+++ src/bin/math.rs
@@ -1,16 +1,18 @@
-fn surprise(p: f64) -> f64 {
+/// Returns the information content in an event with probability p
+fn surprisal(p: f64) -> f64 {
     assert!((0. ..=1.).contains(&p));
-    (1. / p).ln()
+    - p.log2()
 }
 
-fn surprise_naive(p: f64) -> f64 {
+fn odds(p: f64) -> f64 {
     assert!((0. ..=1.).contains(&p));
-    1. / p - 1.
+    p / (1. - p)
 }
 
 fn main() {
+    println!("s : p \u{2192} \u{211D}+");
     for val in (0..=10).map(|i| i as f64 * 0.1) {
-        println!("s1({}) = {}", val, surprise_naive(val));
-        println!("s2({}) = {}", val, surprise(val));
+        println!("sup({:.1}) = {:.3}", val, surprisal(val));
+        println!("odd({:.1}) = {:.3}", val, odds(val));
     }
 }
